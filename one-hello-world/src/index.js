3b9da80071c1721035705087ef2c834acd73c232
busisatya@gmail.com <busisatya@gmail.com>
2022-05-28 15:42:06 -0400

    Seperate App

--- one-hello-world/src/index.js
+++ one-hello-world/src/index.js
@@ -2,14 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./style.css";
 
-function App() {
-  return (
-    <div>
-      <h1>Hello World</h1>
-      <button className="button">Hi</button>
-    </div>
-  );
-}
+//This is for importing the document
+import App from "./App";
 
 //Now in the ReactDOM, we fetch element by id which is root and render APP in it.
 //App is a function which is a component in React and so we move used the component notation
